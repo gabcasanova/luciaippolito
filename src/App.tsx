2dff9505a4e80f483a6acce47028addee55bcd0f
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-07-01 16:35:44 -0300

    configured i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './i18n';
 import { HashRouter, Route, Routes } from "react-router";
 import Home from "./pages/home";
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,12 @@
 //import React from 'react'
 
+import { useTranslation } from "react-i18next"
+
 const Home = () => {
+  const { t } = useTranslation()
+
   return (
-    <div>home</div>
+    <div>{ t("test.test") }</div>
   )
 }
 
