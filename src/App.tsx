b5a103c5eb235dd9e14062f20c5cbb8731a3d930
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-07-25 10:22:49 -0300

    Experience page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './i18n';
 import { HashRouter, Route, Routes } from "react-router";
 import Homepage from './pages/Homepage';
+import Experience from './pages/Experience';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/"    element={<Homepage />} />
+        <Route path="/exp" element={<Experience />} />
       </Routes>
     </HashRouter>
   )
